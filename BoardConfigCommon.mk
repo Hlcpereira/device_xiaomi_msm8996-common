Cosmic-Devices/android_device_xiaomi_

msm8996-common: Add dumpstate static HAL
BUG: 27799254

Change-Id: I70595f64283b06e4ce100caec64aeb76ec6c25d7
Signed-off-by: norman_liu <norman_liu@htc.com>
Signed-off-by: Sebastian <xdzienny@gmail.com>

BoardConfigCommon.mk
@@ -136,6 +136,9 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 
+# Dumpstate
+BOARD_HAL_STATIC_LIBRARIES := libdumpstate.gemini
+
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 

Hlcpereira/device_xiaomi_msm8996-common

Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

BoardConfigCommon.mk
@@ -133,6 +133,9 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 
+# Dumpstate
+BOARD_HAL_STATIC_LIBRARIES := libdumpstate.msm8996
+
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 